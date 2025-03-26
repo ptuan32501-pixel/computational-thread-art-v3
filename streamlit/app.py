88acb550bb3161d1b3efa305983e12752ec03f23
callummcdougall <cal.s.mcdougall@gmail.com>
2025-03-26 19:15:07 +0000

    update paths

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,6 +12,13 @@ st.set_page_config(page_title="Thread Art Generator", page_icon="🧵", layout="
 
 # Add parent directory to path so we can import the required modules
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parent_dir)
+# for path in Path(parent_dir).iterdir():
+#     st.write(path)
+#     if not path.is_file():
+#         for subpath in path.iterdir():
+#             st.write("sub: ", subpath)
+
 os.chdir(parent_dir)
 
 from image_color import Img, ThreadArtColorParams
